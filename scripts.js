d3ca3d5bd3e19315fd894cceba119fc9d6f2108c
Pont3sx <samily.pontes09@aluno.ifce.edu.br>
2025-07-18 21:44:07 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function CliqueiNoBotao() {
+    let textoInput = document.querySelector(".input-animacao").value
+    let button = document.querySelector(".botao")
+    button.disabled = true;
+    
+}
